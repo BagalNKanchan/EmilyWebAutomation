9bc98ba2f112f7cb6bd1e474d68159803b427fc7
KanchanNBagal <kanchan@weareloup.com>
2018-08-15 15:35:22 +1000

    Adding code

---
+++ src/main/java/EmilyWebAutomation/EmilyWebAutomation/LoginPage.java
@@ -0,0 +1,26 @@
+package EmilyWebAutomation.EmilyWebAutomation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Hello world!
+ *
+ */
+public class LoginPage 
+{
+	 private String DEFAULT_URL = "https://us-emily-test.azurewebsites.net/";
+	  private WebDriver driver;
+	  private WebElement element;
+
+	  public LoginPage() {
+	//    this.driver = RunTest.driver;
+	  }
+
+ public void openURL() {
+	 System.out.println("Login button click");
+ 	driver.get(DEFAULT_URL);
+ 	 System.out.println(driver.getTitle());
+ }
+}
